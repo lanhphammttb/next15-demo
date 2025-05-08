f77f55ac669ee0e1e77b58fb73206693771d7aef
lanhpt <lanhphammttb@gmail.com>
2025-05-08 11:07:07 +0700

    two-test

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,17 +1,15 @@
-import ProfileHeader from "@/components/profile/ProfileHeader";
-import ProfileInfo from "@/components/profile/ProfileInfo";
-import SocialLinks from "@/components/profile/SocialLinks";
-
 export default function ProfilePage() {
   return (
-    <main className="min-h-screen bg-gray-100 py-12 px-4">
-      <section className="max-w-xl mx-auto bg-white rounded-3xl shadow-lg overflow-hidden">
-        <ProfileHeader />
-        <div className="pt-20 pb-10 text-center px-6">
-          <ProfileInfo />
-          <SocialLinks />
-        </div>
-      </section>
-    </main>
+    <section className="p-4">
+      <div className="container">
+        <h1 className="text-2xl font-bold">About Page</h1>
+        <p className="w-1/2 font-medium py-2">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae eligendi ut
+          necessitatibus nulla modi dolorem dicta animi. Praesentium aliquid, ducimus libero omnis
+          cumque quisquam nesciunt, exercitationem similique expedita recusandae nam reprehenderit
+          earum officiis ad nisi optio laboriosam natus. Id, dolorum.
+        </p>
+      </div>
+    </section>
   );
 }
